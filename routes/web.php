537ae0eed303ae0d4a565932e0bc4d76d96a0f7a
Nirmaljeet <nirmaljeets20@gmail.com>
2024-01-24 16:27:28 +0530

    code merge for profile update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomAuthController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,9 +12,16 @@ use App\Http\Controllers\CustomAuthController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('dashboard', [CustomAuthController::class, 'dashboard'])->name('dashboard');
 Route::get('login', [CustomAuthController::class, 'index'])->name('login');
 Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
 Route::get('register', [CustomAuthController::class, 'registration'])->name('register');
 Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
 Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
+
+Route::group(['middleware' => 'auth'],function(){
+    Route::get('dashboard', [CustomAuthController::class, 'dashboard'])->name('dashboard');
+    Route::get('profile',[ProfileController::class,'index'])->name('profile');
+    Route::put('profile/password-update',[ProfileController::class,'passwordUpdate'])->name('profile.password-update');
+    Route::put('profile/update',[ProfileController::class,'update'])->name('profile.update');
+
+});
